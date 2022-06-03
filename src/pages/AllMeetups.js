92a868b2cfee437663154c0311f442bd1c3f4efd
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-06-03 18:55:21 +0200

    Adding more components

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,3 +1,4 @@
+import MeetupList from "../components/meetups/MeetupList";
 const DUMMY_DATA = [
   {
     id: "m1",
@@ -23,11 +24,7 @@ function AllMeetupsPage() {
   return (
     <section>
       <h1>All Meetups</h1>
-      <ul>
-        {DUMMY_DATA.map((meetup) => {
-          return <li key={meetup.id}>{meetup.title}</li>;
-        })}
-      </ul>
+      <MeetupList meetups={DUMMY_DATA} />
     </section>
   );
 }
