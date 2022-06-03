b962024fa7d6add775f60246a0cad94500ae056b
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-06-03 18:49:55 +0200

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
+import { Route, Routes } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetups";
+import FavoritesPage from "./pages/Favorites";
 function App() {
+  // localhost:3000/
   return (
     <div>
       Hello
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/new-meetup" element={<NewMeetupPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
     </div>
   );
 }
-
 export default App;
