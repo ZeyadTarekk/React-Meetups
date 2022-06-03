fef77a3d278e8351b44f6ce9a83f57e699382d20
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-06-03 19:08:20 +0200

    setting up firebase and sending POST req

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
-import NewMeetupPage from "./pages/NewMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
 import Layout from "./components/layout/Layout";
 function App() {
---
+++ src/pages/NewMeetup.js
@@ -0,0 +1,23 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+function NewMeetupPage() {
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-second-app-71f0e-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
+}
+export default NewMeetupPage;
